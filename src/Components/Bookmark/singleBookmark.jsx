8d1359448103dd75e97e9aa6086fe161f5a83180
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-01-08 10:36:58 +0600

    Bookmark add

--- src/Components/Bookmark/singleBookmark.jsx
+++ src/Components/Bookmark/singleBookmark.jsx
@@ -12,7 +12,7 @@ const SingleBookmark = ({bookmark, handleDelete}) => {
                 <h2 className='text-center md:text-[22px]  font-semibold'>{title}</h2>
                 <p className='mb-1 md:text-x[20px] text-gray-600 text-center'>{category}</p>
                 <p className='text-center w-70 mb-2'>{description.slice(0,30)}...</p>
-                 <Link to={`/news/${News_Id}`} className='ml-[42%] bg-purple-500 p-1 rounded-lg md:font-bold text-white'>Details</Link>
+                 <Link to={`/news/${News_Id}`} className='ml-[42%] bg-purple-400 p-1 rounded-lg md:font-bold text-white'>Details</Link>
                  
             </div>
             <button onClick={() => handleDelete(_id)}><img className='w-6' src="https://i.ibb.co/Lxk2gbr/clipart842915.png" alt="" /></button>
