0dcb69d9184802182a41cf789694dbf14f607e0d
AbdullahAlHemel <abdullahalhemel100@gmail.com>
2024-01-08 10:41:22 +0600

    ErrorPage

--- src/Components/Errorpage/ErrorPage.jsx
+++ src/Components/Errorpage/ErrorPage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ErrorPage = () => {
     return (
         <div><div className='m-auto mt-55 w-44'>
-            <h2 className='text-red-400 font-semibold text-center text-[40px]'>Error 404</h2>
+            <h2 className='text-red-500 font-semibold text-center text-[40px]'>Error 404</h2>
             <button className='btn ml-12 mt-2'><Link>Home</Link></button></div>
         </div>
     );
